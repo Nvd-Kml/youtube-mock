8e4e9a93a64fdc9e72effbc2aecbdfdf454f9adf
Nived Kamal <49567451+Nvd-Kml@users.noreply.github.com>
2024-05-01 00:53:53 +0530

    Initial push

---
+++ scripts/youtube.js
@@ -0,0 +1,7 @@
+
+
+const tweetButton = document.querySelector('.js-tweet-button');
+
+        tweetButton.addEventListener('click', function() {
+        window.open("https://twitter.com/", "_blank");
+    });
